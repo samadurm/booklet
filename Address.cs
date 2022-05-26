5a69c03679a2b22c119f761bf082f91dd19491c3
Micah Samaduroff <msamaduroff@gmail.com>
2022-05-25 20:09:50 -0700

    Implemented properties and constructor for address

--- Address.cs
+++ Address.cs
@@ -1,4 +1,24 @@
 namespace BookletCore
 {
+    class Address 
+    {
+        public string StreetName { get; set; }
+        public string? AptNumber { get; set; }
+        public string City{ get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string ZipCode;
 
+        public Address(string streetName, string? aptNumber, string city, string state, string zipCode)
+        {
+            if (aptNumber != null)
+            {
+                AptNumber = aptNumber;
+            }
+            StreetName = streetName; 
+            City = city;
+            State = state;
+            ZipCode = zipCode;
+        }
+    }
 }
