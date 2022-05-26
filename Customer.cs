7ffef4dec2f901bedde8c1edd46ea44b4105fa14
Micah Samaduroff <msamaduroff@gmail.com>
2022-05-25 20:38:30 -0700

    added basic customer class

--- Customer.cs
+++ Customer.cs
@@ -1,4 +1,20 @@
 namespace BookletCore
 {
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public Address? Address { get; set; }
 
+        Customer(string firstName, string lastName, string phoneNumber, string email, Address? address = null)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            Address = address;
+        }
+    }
 }
